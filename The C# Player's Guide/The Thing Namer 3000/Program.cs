c5bf559b271e4ab70c296f404b097e9118897607
Povilas 'Povka' Frismantas <f.povilas@gmail.com>
2023-06-16 13:15:54 +0300

    Level 4 Comments Completed
    - Added 1 Solution to challange

---
+++ The C# Player's Guide/The Thing Namer 3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // Reading a thing from a user
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); /* Readidng description of thing */
+string c = "of Doom"; // Creating variable which holds string "of Doom"
+string d = "3000"; // Creating variable which holds string "3000"
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
