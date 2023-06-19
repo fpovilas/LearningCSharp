4a6591260a96b96b2f4c13b8f36a9d70c9bdc01b
Povilas 'Povka' Frismantas <f.povilas@gmail.com>
2023-06-19 13:56:45 +0300

    Completed Level 6
    - Created 2 solutions for coding challanges

---
+++ The C# Player's Guide/The Variable Shop Returns/Program.cs
@@ -0,0 +1,69 @@
+﻿// Declaring variables
+int skaicius = 1;
+uint uSkaicius = 2;
+
+byte labaiMazasSkaicius = 3;
+sbyte sLabaiMazasSkaicius = 4;
+
+short mazasSkaicius = 5;
+ushort uMazasSkaicius = 6;
+
+long didelisSkaicius = 7;
+ulong uDidelisSkaicius = 8;
+
+float netikslusKablelis = 9.0f;
+double tikslusKablelis = 10.0;
+decimal labaiTikslusKablelis = 11.0m;
+
+string tekstas = "Tekstas";
+char simbolis = 'S';
+
+bool taipNe = true;
+
+// Display initialized values
+Console.WriteLine(skaicius);
+Console.WriteLine(uSkaicius);
+Console.WriteLine(labaiMazasSkaicius);
+Console.WriteLine(sLabaiMazasSkaicius);
+Console.WriteLine(mazasSkaicius);
+Console.WriteLine(uMazasSkaicius);
+Console.WriteLine(didelisSkaicius);
+Console.WriteLine(uDidelisSkaicius);
+Console.WriteLine(netikslusKablelis);
+Console.WriteLine(tikslusKablelis);
+Console.WriteLine(labaiTikslusKablelis);
+Console.WriteLine(tekstas);
+Console.WriteLine(simbolis);
+Console.WriteLine(taipNe + "\n");
+
+// Assinign new values
+skaicius = 10;
+uSkaicius = 20;
+labaiMazasSkaicius = 30;
+sLabaiMazasSkaicius = 40;
+mazasSkaicius = 50;
+uMazasSkaicius = 60;
+didelisSkaicius = 70;
+uDidelisSkaicius = 80;
+netikslusKablelis = 90.0f;
+tikslusKablelis = 100.0;
+labaiTikslusKablelis = 110.0m;
+tekstas = "Zodis";
+simbolis = 'Z';
+taipNe = false;
+
+// Display updated values
+Console.WriteLine(skaicius);
+Console.WriteLine(uSkaicius);
+Console.WriteLine(labaiMazasSkaicius);
+Console.WriteLine(sLabaiMazasSkaicius);
+Console.WriteLine(mazasSkaicius);
+Console.WriteLine(uMazasSkaicius);
+Console.WriteLine(didelisSkaicius);
+Console.WriteLine(uDidelisSkaicius);
+Console.WriteLine(netikslusKablelis);
+Console.WriteLine(tikslusKablelis);
+Console.WriteLine(labaiTikslusKablelis);
+Console.WriteLine(tekstas);
+Console.WriteLine(simbolis);
+Console.WriteLine(taipNe);
---
+++ The C# Player's Guide/The Variable Shop/Program.cs
@@ -0,0 +1,37 @@
+﻿// Declaring variables
+int skaicius = 1;
+uint uSkaicius = 2;
+
+byte labaiMazasSkaicius = 3;
+sbyte sLabaiMazasSkaicius = 4;
+
+short mazasSkaicius = 5;
+ushort uMazasSkaicius = 6;
+
+long didelisSkaicius = 7;
+ulong uDidelisSkaicius = 8;
+
+float netikslusKablelis = 9.0f;
+double tikslusKablelis = 10.0;
+decimal labaiTikslusKablelis = 11.0m;
+
+string tekstas = "Tekstas";
+char simbolis = 'S';
+
+bool taipNe = true;
+
+// Displaying declared variables
+Console.WriteLine(skaicius);
+Console.WriteLine(uSkaicius);
+Console.WriteLine(labaiMazasSkaicius);
+Console.WriteLine(sLabaiMazasSkaicius);
+Console.WriteLine(mazasSkaicius);
+Console.WriteLine(uMazasSkaicius);
+Console.WriteLine(didelisSkaicius);
+Console.WriteLine(uDidelisSkaicius);
+Console.WriteLine(netikslusKablelis);
+Console.WriteLine(tikslusKablelis);
+Console.WriteLine(labaiTikslusKablelis);
+Console.WriteLine(tekstas);
+Console.WriteLine(simbolis);
+Console.WriteLine(taipNe);
